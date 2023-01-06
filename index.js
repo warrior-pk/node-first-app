9abafbc364320241576e5069d616438607f146ee
Piyush Kumar <piyu8h@gmail.com>
2023-01-06 22:47:48 +0530

    Content Added To HomePage

--- index.js
+++ index.js
@@ -3,34 +3,13 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    // if(req.url === '/'){
-    //     fs.readFile(
-    //         path.join(__dirname, 'public', 'index.html'), 
-    //         (err, content) => {
-    //         if(err) throw err;
-        
-    //         res.writeHead(200, {'Content-Type' : 'text/html'})
-    //         res.end(content);
-    //     })
-    // }
-
-    // if(req.url === '/about'){
-    //     fs.readFile(
-    //         path.join(__dirname, 'public', 'about.html'), 
-    //         (err, content) => {
-    //         if(err) throw err;
-        
-    //         res.writeHead(200, {'Content-Type' : 'text/html'})
-    //         res.end(content);
-    //     })
-    // }
 
     //  Build File path
     let filePath = path.join(__dirname, 'public', req.url === '/'?
     'index.html' : req.url);
-
     // console.log(filePath);
-    let extName = path.extname(filePath);
+
+    let extName = path.extname(filePath);   //extension name
 
     //  Initial content type
     let contentType = 'text/html';
@@ -75,6 +54,7 @@ const server = http.createServer((req, res) => {
     });
     
 });
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT , () => console.log(`Server running on port ${PORT}`));
